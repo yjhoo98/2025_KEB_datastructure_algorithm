58e5faf16364d0f608849f7b879dbba5e2754b5e
yjhoo98 <yjhoo98@gmail.com>
2025-02-17 15:57:53 +0900

    v1.0

--- day06.py
+++ day06.py
@@ -1,31 +1,9 @@
-memo=dict()
-def fibonacci_memo(n)->int:
-    if n in memo:
-        return memo[n]
-    elif n<=1:
-        return n
-    else:
-        memo[n]=fibonacci_memo(n-2)+fibonacci_memo(n-1)
-        return memo[n]
-def fibonacci_recursion(n):
-    """
-    fibonacci sequence using recurion
-    :param n:integer
-    :return:fibonacci sequence
-    """
-    if n==0:
-        return 0
-    elif n==1:
-        return 1
-    else:
-        return fibonacci_recursion(n-1)+fibonacci_recursion(n-2)
-def fibonacci_repetition(n)->int:
-    n_list=[0,1]
-    for i in range(n+1):
-        n_list.append(n_list[i]+n_list[i+1])
-    return n_list[n]
-num=int(input("number:"))
-print(fibonacci_repetition(num))
-print(fibonacci_recursion(num))
-n=int(input())
-print(fibonacci_memo(n))
+import numpy as np
+
+narray = np.array([1, 3.1, 2, 9])
+print(type(narray), type(narray[2]), type(narray[3]))
+
+array = [9, -11, '8', 7]
+print(array[0], array[1], array[2], array[3])
+print(type(array), type(array[2]), type(array[3]))
+print(id(array[0]),id(array[1]),id(array[2]),id(array[3]))
