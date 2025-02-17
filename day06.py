b46c74ad499c5889e5ac37d84a34c06f383174a5
yjhoo98 <yjhoo98@gmail.com>
2025-02-17 16:30:12 +0900

    v1.1 file io

--- day06.py
+++ day06.py
@@ -1,9 +1,11 @@
-import numpy as np
 
-narray = np.array([1, 3.1, 2, 9])
-print(type(narray), type(narray[2]), type(narray[3]))
-
-array = [9, -11, '8', 7]
-print(array[0], array[1], array[2], array[3])
-print(type(array), type(array[2]), type(array[3]))
-print(id(array[0]),id(array[1]),id(array[2]),id(array[3]))
+try:
+    file=input("파일명:")
+    fp=open(file,'r')
+    readme_list=fp.readlines()
+    rls=readme_list[0].split('_')
+    print(readme_list)
+    print(rls)
+    fp.close()
+except FileNotFoundError as err:
+    print(f'{file} is not exist.{err}')
