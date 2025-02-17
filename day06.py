0ae3b4febc7470c2a445d03069a6561282b7e470
yjhoo98 <yjhoo98@gmail.com>
2025-02-17 14:15:18 +0900

    v0.8 poly

--- day06.py
+++ day06.py
@@ -1,17 +1,17 @@
 def print_poly(f_x,t_x) -> str:
-    # term = len(f_x) - 1
+
     poly_expression = "f(x) = "
 
-    for i in range(len(fx)-1):
+    poly_expression=f'{f_x[0]}x^{t_x[0]}'
+    for i in range(1,len(fx)-2):
         coefficient = f_x[i]
         term=t_x[i]
         if coefficient!=0:
             if coefficient > 0:
                 poly_expression = poly_expression + "+"
-            # elif coefficient==0:
-            #     poly_expression = poly_expression
+
             poly_expression = poly_expression + f'{coefficient}x^{term} '
-            # term = term - 1
+
         elif coefficient==0:
             poly_expression = poly_expression
     if f_x[len(f_x)-1]==0:
@@ -30,7 +30,7 @@ def calculation_poly(x_value, f_x,t_x) -> int:
         coefficient = f_x[i]
         term = t_x[i]
         return_value += coefficient * pow(x_value, term)
-        # term = term - 1
+
 
     return return_value
 
