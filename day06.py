970884763b811f7b125f62caeb326e928f853f03
yjhoo98 <yjhoo98@gmail.com>
2025-02-17 09:43:07 +0900

    v0.2 day bit operation

--- day06.py
+++ day06.py
@@ -1,6 +1,15 @@
 def is_even(n)->bool:
+    """
+    짝수 판정 함수
+    :param n: integer
+    :return: even->return True/odd-> return False
+    """
     if n%2==0:
         return True
     return False
-n=int(input())
-print(is_even(n))
+
+a=10
+b=11
+print(a&b)
+print(a|b)
+print(a^b)
