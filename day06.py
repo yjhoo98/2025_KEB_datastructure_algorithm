325aedf1ec7a4fe9834442cf2f17fc0970d11c72
yjhoo98 <yjhoo98@gmail.com>
2025-02-17 17:49:43 +0900

    v1.2

--- day06.py
+++ day06.py
@@ -1,11 +1,34 @@
+#)guess number 예제 자동화,로그파일(guess.txt)
+import random
 
-try:
-    file=input("파일명:")
-    fp=open(file,'r')
-    readme_list=fp.readlines()
-    rls=readme_list[0].split('_')
-    print(readme_list)
-    print(rls)
-    fp.close()
-except FileNotFoundError as err:
-    print(f'{file} is not exist.{err}')
+def guess_num(low,high,answer,chance)->int:
+    mid=(low+high)//2
+    print(f'guess numberis {mid}')
+    fp.write(f'guess numberis {mid}')
+    while chance != 0:
+
+        if mid == answer:
+            # print(f'You win. Answer is {answer}')
+            with open('guess.txt', 'w') as fp:
+                fp.write(f'You win. Answer is {answer}(chance:{chance})')
+            break
+        elif mid > answer:
+            chance = chance - 1
+            print(f'{mid} is bigger. Chance left : {chance}')
+            fp.write(f'{mid} is bigger. Chance left : {chance}')
+            return guess_num(low,mid-1,answer,chance)
+        else:
+            chance = chance - 1
+            print(f'{mid} is lower. Chance left : {chance}')
+            fp.write(f'{mid} is lower. Chance left : {chance}')
+            return guess_num(mid+1,high,answer,chance)
+    else:
+        print(f'You lost. Answer is {answer}')
+        fp.write(f'You lost. Answer is {answer}')
+if __name__=="__main__":
+    low=1
+    high=100
+    ans=random.randint(1,100)
+    chance=7
+    with open('guess.txt', 'w') as fp:
+        guess_num(low, high, ans, chance)
