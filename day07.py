072ece62b9b04dcbb102fca4f09368343c1a7531
yjhoo98 <yjhoo98@gmail.com>
2025-02-18 11:18:38 +0900

    v1.7 day07 linked list print

--- day07.py
+++ day07.py
@@ -13,8 +13,15 @@ class LinkedList:
         while current.next:#if next node exist
             current=current.next#move
         current.next=Node(data)
+    def __str__(self):
+        node=self.head
+        while node is not None:
+            print(node.data)
+            node=node.next
+        return "end"
 if __name__=="__main__":
     l=LinkedList()
     l.append(7)
     l.append(-11)
     l.append(8)
+    print(l)
