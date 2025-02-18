1aace7ac670d54fe91075d96beb92f1c7b9459b3
yjhoo98 <yjhoo98@gmail.com>
2025-02-18 11:31:24 +0900

    v1.9 day07 linkedlist search

--- day07.py
+++ day07.py
@@ -1,8 +1,35 @@
-from collections import deque
-d=deque()
-d.append(7)
-d.append(-11)
-d.append(8)
+class Node:
+    def __init__(self,data,next=None):
+        self.data=data
+        self.next=next
+class LinkedList:
+    def __init__(self):
+        self.head=None
+    def append(self,data):
+        if not self.head:
+            self.head=Node(data)
+            return
+        current=self.head
+        while current.next:#if next node exist
+            current=current.next#move
+        current.next=Node(data)
+    def search(self,target):
+        current=self.head
+        while current.next:
+            if current.data==target:
+                return True
+            else:
+                current=current.next
+        return False
+    def __str__(self):
+        node=self.head
+        while node is not None:
+            print(node.data)
+            node=node.next
+        return "end"
 if __name__=="__main__":
-    for item in d:
-        print(item)
+    l=LinkedList()
+    l.append(7)
+    l.append(-11)
+    l.append(8)
+    print(l)
