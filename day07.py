d2e66e9ad548e1abbb4f51c69e74d219d464bd79
yjhoo98 <yjhoo98@gmail.com>
2025-02-18 11:35:08 +0900

    v2.0 day07 linkedlist search

--- day07.py
+++ day07.py
@@ -1,3 +1,4 @@
+import random
 class Node:
     def __init__(self,data,next=None):
         self.data=data
@@ -29,7 +30,10 @@ class LinkedList:
         return "end"
 if __name__=="__main__":
     l=LinkedList()
-    l.append(7)
-    l.append(-11)
-    l.append(8)
-    print(l)
+    i=0
+    while i<20:
+        n=random.randint(1,20)
+        l.append(n)
+        print(n,end=' ')
+        i=i+1
+    print(l.search(10))
