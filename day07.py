d33064212dc6d027b9666ab4a6d25e0c72f26ee3
yjhoo98 <yjhoo98@gmail.com>
2025-02-18 16:50:38 +0900

    v2.4 day07 queue

--- day07.py
+++ day07.py
@@ -1,39 +1,69 @@
+def is_queue_full() :
+    global size, queue, front, rear
+    if rear!=(size-1):
+        return False
+    elif (front==-1) and (rear==size-1):
+        return True
+    else:
+        for i in range(front+1,size):
+            queue[i-1]=queue[i]
+            queue[i]=None
+        front-=1
+        rear-=1
+        return False
 
-class Node:
-    def __init__(self,data,next=None):
-        self.data=data
-        self.next=next
-class Queue:
-    def __init__(self):
-        self.front=None
-        self.rear=None
-        self._size=0
-    def enqueue(self,data):
-        self._size=self._size+1
-        node=Node(data)
-        if self.rear is None:
-            self.front=node
-            self.rear=node
+def is_queue_empty() :
+    global size, queue, front, rear
+    if front == rear:
+        return True
+    else :
+        return False
+
+def en_queue(data) :
+    global size, queue, front, rear
+    if is_queue_full():
+        print("큐가 꽉 찼습니다.")
+        return
+    rear += 1
+    queue[rear] = data
+
+def de_queue() :
+    global size, queue, front, rear
+    if is_queue_empty():
+        print("큐가 비었습니다.")
+        return None
+    front += 1
+    data = queue[front]
+    queue[front] = None
+    return data
+
+def peek() :
+    global size, queue, front, rear
+    if is_queue_empty():
+        print("큐가 비었습니다.")
+        return None
+    return queue[front+1]
+
+
+size = int(input("큐의 크기를 입력 : "))
+queue = [None for _ in range(size)]
+front = rear = -1
+
+if __name__ == "__main__" :
+    while True:
+        menu = input("삽입(E)/삭제(D)/확인(P)/종료(X) : ")
+        if menu == 'X' or menu == 'x':
+            break
+        elif menu== 'E' or menu == 'e' :
+            data = input("입력할 데이터 : ")
+            en_queue(data)
+            print(queue)
+        elif menu== 'D' or menu == 'd' :
+            print("삭제된 데이터 : ", de_queue())
+            print(queue)
+        elif menu== 'P' or menu == 'p' :
+            print("확인된 데이터 : ", peek())
+            print(queue)
         else:
-            self.rear.next=node
-            self.rear=node
-    def dequeue(self):
-        if self.front is None:
-            raise IndexError('dequeue from empty queue')
-        self._size-=1
-        temp=self.front
-        self.front=self.front.next
-        if self.front is None:
-            self.rear=None
-        return temp.data
-    def size(self)->int:
-        return self._size
-
-if __name__=="__main__":
-    q=Queue()
-    q.enqueue(7)
-    q.enqueue(-11)
-    q.enqueue(8)
-    for i in range(q.size()+1):
-        print(q.dequeue())
-    print(q.size())
+            print("입력이 잘못됨")
+    print("프로그램 종료!")
