9a372ca9a7dd281a32dfc3f876cda7c5b2028435
yjhoo98 <yjhoo98@gmail.com>
2025-02-18 17:40:13 +0900

    v2.6 day07 circular linkedlist assignment

--- day07.py
+++ day07.py
@@ -1,15 +1,9 @@
 def is_queue_full() :
     global size, queue, front, rear
-    if rear!=(size-1):
-        return False
-    elif (front==-1) and (rear==size-1):
+    if (rear+1)%size==front:
         return True
+
     else:
-        for i in range(front+1,size):
-            queue[i-1]=queue[i]
-            queue[i]=None
-        front-=1
-        rear-=1
         return False
 def is_queue_empty() :
     global size, queue, front, rear
@@ -23,7 +17,7 @@ def en_queue(data) :
     if is_queue_full():
         print("큐가 꽉 찼습니다.")
         return
-    rear += 1
+    rear = (rear+1)%size
     queue[rear] = data
 
 def de_queue() :
@@ -31,7 +25,7 @@ def de_queue() :
     if is_queue_empty():
         print("큐가 비었습니다.")
         return None
-    front += 1
+    front = (front+1)%size
     data = queue[front]
     queue[front] = None
     return data
@@ -41,12 +35,12 @@ def peek() :
     if is_queue_empty():
         print("큐가 비었습니다.")
         return None
-    return queue[front+1]
+    return queue[(front+1)%size]
 
 
 size = int(input("큐의 크기를 입력 : "))
 queue = [None for _ in range(size)]
-front = rear = -1
+front = rear = 0
 
 if __name__ == "__main__" :
     while True:
