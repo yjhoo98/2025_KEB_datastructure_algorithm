1a85a7c0644d1e288b6a872331dc8c9e0dbcf3c6
yjhoo98 <yjhoo98@gmail.com>
2025-02-19 16:47:00 +0900

    v3.3 ãbst by functon

--- day08.py
+++ day08.py
@@ -1,17 +1,91 @@
-def dfs(g, i, visited):
-    visited[i] = 1
-    print(chr(ord('A')+i), end=' ')
-    for j in range(len(g)):
-        if g[i][j] == 1 and not visited[j]:
-            dfs(g, j, visited)
+class TreeNode:
+	def __init__(self):
+		self.left = None
+		self.data = None
+		self.right = None
+def insert_tree(root,node,value=[]):
 
-graph = [
-    [0, 0, 1, 1, 0],
-    [0, 0, 1, 0, 0],
-    [1, 1, 0, 1, 1],
-    [1, 0, 1, 0, 0],
-    [0, 0, 1, 0, 0]
-]
+    node.data = value[0]
+    for value in value[1:]:
+        node = TreeNode()
+        node.data = value
+        current = root
+        while True:
+            if value < current.data:
+                if current.left is None:
+                    current.left = node
+                    break
+                current = current.left  # move
+            else:
+                if current.right is None:
+                    current.right = node
+                    break
+                current = current.right  # move
+    print("complete binary search tree")
+def search_value(root,value):
+    current=root
+    while True:
+        if value == current.data:
+            print(f'{value}을(를) 찾았습니다.')
+            break
+        elif value < current.data:
+            if current.left is None:
+                print(f"{value}이(가) 존재하지 않습니다.")
+                break
+            current = current.left
+        else:
+            if current.right is None:
+                print(f"{value}이(가) 존재하지 않습니다.")
+                break
+            current = current.right
 
-visited = [0] * len(graph)
-dfs(graph, 4, visited)
+def delete_value(root,value):
+    current=root
+    parent = None
+    parent = root
+    while True:
+        if value == current.data:
+            if current.left is None and current.right is None:
+                if parent.left == current:
+                    parent.left = None
+                else:
+                    parent.right = None
+                del (current)
+            elif current.left is not None and current.right is None:
+                if parent.left == current:
+                    parent.left = current.left
+                else:
+                    parent.right = current.left
+                del (current)
+            elif current.left is None and current.right is not None:
+                if parent.left == current:
+                    parent.left = current.right
+                else:
+                    parent.right = current.right
+                del (current)
+            elif current.left is not None and current.right is not None:
+                pass
+            print(f"{value}이(가) 삭제됨.")
+            break
+        elif value < current.data:
+            if current.left is None:
+                print(f'{value}이(가) 없음')
+                break
+            parent = current
+            current = current.left
+        else:
+            if current.right is None:
+                print(f'{value}이(가) 없음')
+                break
+            parent = current
+            current = current.right
+if __name__=="__main__":
+    groups=['블랙핑크','레드벨벳','마마무','에이핑크','걸스데이','트와이스']
+    root=None
+    node=TreeNode()
+    root=node
+    insert_tree(root,node,groups)
+    find_group=input()
+    search_value(root,find_group)
+    deleteName=input()
+    delete_value(root,deleteName)
