133ef75df3bae5cfd274fe176d74121b12486354
yjhoo98 <yjhoo98@gmail.com>
2025-02-21 10:23:15 +0900

    v4.5

--- day10.py
+++ day10.py
@@ -1,7 +1,7 @@
 def bubble_sort(a_list):
     list_length=len(a_list)-1
     for i in range(list_length):
-        for j in range(list_length):
+        for j in range(list_length-i):
             if a_list[j]>a_list[ j+1]:
                 a_list[j],a_list[j+1]=a_list[j+1],a_list[j]
     return a_list
