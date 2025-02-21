c92eb0c771ed2848039b519329ff6976981e653e
yjhoo98 <yjhoo98@gmail.com>
2025-02-21 10:27:52 +0900

    v4.6 bubble sort3

--- day10.py
+++ day10.py
@@ -1,8 +1,13 @@
 def bubble_sort(a_list):
     list_length=len(a_list)-1
     for i in range(list_length):
+        no_swap=True
         for j in range(list_length-i):
+
             if a_list[j]>a_list[ j+1]:
                 a_list[j],a_list[j+1]=a_list[j+1],a_list[j]
+                no_swap=False
+        if no_swap:
+            return a_list
     return a_list
 print(bubble_sort([8,-11,9,1]))
