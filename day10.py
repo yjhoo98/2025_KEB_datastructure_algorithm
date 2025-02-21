c31b468611ad431af51ba03c2afe38df1dd51a13
yjhoo98 <yjhoo98@gmail.com>
2025-02-21 13:19:15 +0900

    v4.8

--- day10.py
+++ day10.py
@@ -1,23 +1,44 @@
+import time
+import random
+
+def time_decorator(func):
+    def wrapper(*arg):
+        s = time.time()
+        r = func(*arg)
+        e = time.time()
+        print(f'실행시간 : {e - s}초')
+        return r
+    return wrapper
+
+
+@time_decorator
 def insertion_sort(l):
-    for i in range(1,len(l)):
-        value=l[i]
-        while i>0 and l[i-1]>value:
-            l[i]=l[i-1]
-            i=i-1
-
-        l[i]=value
+    for i in range(1, len(l)):
+        value = l[i]
+        while i > 0 and l[i-1] > value:
+            l[i] = l[i-1]
+            i = i - 1
+            #print(i, end=' ')
+        l[i] = value
     return l
-def bubble_sort(a_list):
-    list_length=len(a_list)-1
-    for i in range(list_length):
-        no_swap=True
-        for j in range(list_length-i):
 
-            if a_list[j]>a_list[ j+1]:
-                a_list[j],a_list[j+1]=a_list[j+1],a_list[j]
-                no_swap=False
+
+@time_decorator
+def bubble_sort(l):
+    for i in range(len(l) - 1):
+        no_swap = True
+        for j in range(len(l) - 1 - i):
+            if l[j] > l[j+1]:
+                l[j], l[j + 1] = l[j+1], l[j]
+                no_swap = False
+                #print(j, end=' ')
         if no_swap:
-            return a_list
-    return a_list
-print(bubble_sort([8,-11,9,1]))
-print(insertion_sort([8,-11,9,1]))
+            return l
+    return l
+
+
+
+lists1 = [random.randint(1, 100000) for _ in range(10000)]
+lists2 = lists1.copy()
+bubble_sort(lists1)
+insertion_sort(lists2)
