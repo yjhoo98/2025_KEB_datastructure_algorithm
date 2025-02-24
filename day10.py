e6cd1c83ef2c536dadcf6963933129b786a243f4
yjhoo98 <yjhoo98@gmail.com>
2025-02-24 11:26:18 +0900

    v5.0

--- day10.py
+++ day10.py
@@ -1,63 +1,62 @@
-import time
-import random
-
-def time_decorator(func):
-    def wrapper(*arg):
-        s = time.time()
-        r = func(*arg)
-        e = time.time()
-        print(f'실행시간 : {e - s}초')
-        return r
-    return wrapper
-
-
-@time_decorator
-def insertion_sort(l):
-    for i in range(1, len(l)):
-        value = l[i]
-        while i > 0 and l[i-1] > value:
-            l[i] = l[i-1]
-            i = i - 1
-            #print(i, end=' ')
-        l[i] = value
-    return l
-
-
-@time_decorator
-def bubble_sort(l):
-    for i in range(len(l) - 1):
-        no_swap = True
-        for j in range(len(l) - 1 - i):
-            if l[j] > l[j+1]:
-                l[j], l[j + 1] = l[j+1], l[j]
-                no_swap = False
-                #print(j, end=' ')
-        if no_swap:
-            return l
-    return l
-
-def quick_sort(l):
-    n=len(l)
-    if n<=1:
-        return l
-    pivot=l[n//2]
-    left,mid,right=list(),list(),list()
-    for i in l:
-        if i <pivot:
-            left.append(i)
-        elif i>pivot:
-            right.append(i)
+import sys
+class TreeNode:
+	def __init__(self):
+		self.left = None
+		self.data = None
+		self.right = None
+def insert(root,value):
+    if root is None:
+        node = TreeNode()
+        node.data = value
+        return node
+    if value < root.data:
+        root.left = insert(root.left, value)
+    else:
+        root.right = insert(root.right, value)
+    return root
+def search(root,value):
+    current = root
+    while True:
+        if value == current.data:
+            return current
+        elif value < current.data:
+            current = current.left
         else:
-            mid.append(i)
-    return quick_sort(left)+mid+quick_sort(right)
-
-
-lists1 = [random.randint(1, 100000) for _ in range(10000)]
-lists2 = lists1.copy()
-lists3=lists1.copy()
-bubble_sort(lists1)
-insertion_sort(lists2)
-s = time.time()
-quick_sort(lists3)
-e = time.time()
-print(f'실행시간 : {e - s}초')
+            current = current.right
+    return None
+def delete(root,value):
+    if root is None:
+        return root
+    if value<root.data:
+        root.left=delete(root.left,value)
+    elif value>root.data:
+        root.right=delete(root.right,value)
+    else:
+        if root.left is None and root.right is None:
+            return None
+        elif root.left is None:
+            return root.right
+        elif root.right is None:
+            return root.left
+        else:
+            root.data=min_value(root.right).data
+            root.right=delete(root.right,value)
+    return root
+def min_value(node):
+    current=node
+    while current.left is not None:
+        current=current.left
+    return current
+def post_order(node):
+    if node is None:
+        return
+    post_order(node.left)
+    post_order(node.right)
+    print(f'{node.data} ',end='')
+def josephus(node,k):
+    return node
+if __name__ == "__main__":
+    n,k=map(int,sys.stdin.readline().split())
+    root=None
+    for i in range(n,0,-1):
+        root = insert(root, i)
